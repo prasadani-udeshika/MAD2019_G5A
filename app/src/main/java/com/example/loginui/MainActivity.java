122fac240f96fcf21d691ebaddfc5dce01c74ffb
prasadani-udeshika <prasadani9727@gmail.com>
2019-08-08 16:25:15 +0530

    1st login commit

---
+++ app/src/main/java/com/example/loginui/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.loginui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText etEmail,etPassword;
+    TextView tvRegister;
+    Button btnLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        etEmail =(EditText)findViewById(R.id.et_email);
+        etPassword = (EditText)findViewById(R.id.et_password);
+        btnLogin =(Button)findViewById(R.id.btn_login);
+        tvRegister = (TextView)findViewById(R.id.tv_register);
+
+        tvRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i= new Intent(MainActivity.this,RegisterActivity.class);
+                startActivity(i);
+
+            }
+        });
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+
+
+
+    }
+}
